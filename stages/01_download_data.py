41ce59115749ca62b741ed80827be147660ea165
Sulstice <sharifsuliman1@gmail.com>
2024-07-17 07:38:57 -0500

    added a fix

--- stages/01_download_data.py
+++ stages/01_download_data.py
@@ -6,7 +6,7 @@ import urllib.request
 os.makedirs('downloads', exist_ok=True)
 
 data_links = {
-  'dude_docking': 'https://dude.docking.org/db/subsets/all/all.tar.gz',
+  'dude_docking.tar.gz': 'https://dude.docking.org/db/subsets/all/all.tar.gz',
 }
 
 _ = [ urllib.request.urlretrieve(link, os.path.join('downloads', data)) for data, link in data_links.items() ]
