f1f8b6cffde84b52291b3a9ed1c90f047ba89bec
Sulstice <sharifsuliman1@gmail.com>
2024-07-26 10:51:16 -0500

    SS-1: added the TODO comment

--- stages/02_download_data.sh
+++ stages/02_download_data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO Only downloading *.ism files for now.
+
 mkdir -p ./download
 
 cat list/target_links.txt | xargs -I {} wget -P download/  -nH --cut-dirs=2 -r -l 1 -A '*.ism' 'https://dude.docking.org/targets/{}'
