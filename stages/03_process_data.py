fd50581d78de82a9d8954ddf7735cf5470d33644
Sulstice <sharifsuliman1@gmail.com>
2024-07-26 10:59:43 -0500

    SS-1: removed the try-except block

--- stages/03_process_data.py
+++ stages/03_process_data.py
@@ -18,31 +18,28 @@ for file in files:
 
     if file.match('*.ism'):
 
-        try:
-            protein_dir = str(file).split('/')[1]
+        protein_dir = str(file).split('/')[1]
 
-            names = {
-              0: 'SMILES',
-              1: 'DAT',   # TODO: Figure out what this column is
-              2: 'Inhibition Metric',
-              3: 'Relation',
-              4: 'Value',
-              5: 'Unit',
-              6: 'Unknown', # TODO: Figure out what this column is
-              7: 'Number of Decoys',
-              8: 'SWISS PROT',
-              9: 'Protein'
-            }
+        names = {
+            0: 'SMILES',
+            1: 'DAT',   # TODO: Figure out what this column is
+            2: 'Inhibition Metric',
+            3: 'Relation',
+            4: 'Value',
+            5: 'Unit',
+            6: 'Unknown', # TODO: Figure out what this column is
+            7: 'Number of Decoys',
+            8: 'SWISS PROT',
+            9: 'Protein'
+        }
 
-            df = pd.read_csv(file, sep='\0', header=None)
-            transformed_df = df[0].str.split(' ', n=9, expand=True)
-            transformed_df.columns = list(names.values())
-            protein_dir = pathlib.Path('brick/%s' % protein_dir)
-            protein_dir.mkdir(exist_ok=True)
-            transformed_df.to_parquet(out_file)
+        df = pd.read_csv(file, sep='\0', header=None)
+        transformed_df = df[0].str.split(' ', n=9, expand=True)
+        transformed_df.columns = list(names.values())
+        protein_dir = pathlib.Path('brick/%s' % protein_dir)
+        protein_dir.mkdir(exist_ok=True)
+        transformed_df.to_parquet(out_file)
 
-        except Exception as e:
-          print ("File Not Processed: %s" % file)
     else:
       raise Exception('Unknown File Found: %s' % file)
 
